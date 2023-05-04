13e090bcfb97b4a6a8c18ac8b72262382667d700
TakaoKeita <keita.takao.1112@gmail.com>
2023-05-04 10:15:42 +0900

    feat: check sytax generics

---
+++ src/generics.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+// mapのkeyを取得する関数
+func MapKeys[K comparable, V any](m map[K]V) []K {
+	r := make([]K, 0, len(m))
+	for k := range m {
+		r = append(r, k)
+	}
+	return r
+}
+
+type List[T any] struct {
+	head, tail *element[T]
+}
+
+type element[T any] struct {
+	next *element[T]
+	val  T
+}
+
+// リストに値を追加する関数
+func (lst *List[T]) Push(v T) {
+	if lst.tail == nil {
+		// headを付け替えてtailに変更
+		lst.head = &element[T]{val: v}
+		lst.tail = lst.head
+	} else {
+		lst.tail.next = &element[T]{val: v}
+		lst.tail = lst.tail.next
+	}
+}
+
+// リストの値を取得する関数
+func (lst *List[T]) GetAll() []T {
+	var elems []T
+	for e := lst.head; e != nil; e = e.next {
+		elems = append(elems, e.val)
+	}
+	return elems
+}
+
+func main() {
+	var m = map[int]string{1: "2", 2: "4", 4: "8"}
+
+  // 型推論でintとstringになる。
+	fmt.Println("keys:", MapKeys(m))
+
+	_ = MapKeys[int, string](m)
+
+	lst := List[int]{}
+	lst.Push(10)
+	lst.Push(13)
+	lst.Push(23)
+	// 連結リストの値を取得
+	fmt.Println("list:", lst.GetAll())
+}
